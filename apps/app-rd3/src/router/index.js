cd560c4008660998df93a507520588bdad3c4dde
davidruvolo51 <davidruvolo51@gmail.com>
2022-10-27 16:04:15 +0200

    added title updating; new components

--- apps/app-rd3/src/router/index.js
+++ apps/app-rd3/src/router/index.js
@@ -1,15 +1,46 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
 import HomePage from '../views/HomePage.vue'
+import GetStartedPage from '../views/GetStarted.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
       name: 'home',
-      component: HomePage
+      path: '/',
+      component: HomePage,
+      meta: {
+        title: 'Home'
+      }
+    },
+    {
+      name: 'get-started',
+      path: '/get-started',
+      component: GetStartedPage,
+      meta: {
+        title: 'Get Started'
+      }
+    },
+    {
+      name: 'login',
+      path: '/login',
     }
   ]
+})
+
+// router.beforeEach(async (to, from, next) => {
+//   const response = await fetch('/app-ui-context')
+//   const data = response.json()
+//   if (to.name == 'get-started' && !data.authenticated) {
+//     next({ to: 'login' })
+//   } else {
+//     next()
+//   }
+// })
+
+router.afterEach((to) => {
+  document.title = `${to.meta.title} | RD3` || 'RD3'
 })
 
 export default router
