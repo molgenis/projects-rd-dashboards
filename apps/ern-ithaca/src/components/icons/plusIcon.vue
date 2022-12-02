983896891ac2eabede31066b486bfeebcc1791a1
davidruvolo51 <davidruvolo51@gmail.com>
2022-12-02 11:00:25 +0100

    init new icon

---
+++ apps/ern-ithaca/src/components/icons/plusIcon.vue
@@ -0,0 +1,3 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" width="24" height="24" stroke-width="1.5" stroke="currentColor" class="heroicon heroicon-plus"><path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" /></svg>
+</template>
