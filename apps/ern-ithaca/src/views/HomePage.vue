a155fe7876533c5c31efbed234a757d7466ce8e3
davidruvolo51 <davidruvolo51@gmail.com>
2022-12-02 11:02:17 +0100

    redesigned nav links

--- apps/ern-ithaca/src/views/HomePage.vue
+++ apps/ern-ithaca/src/views/HomePage.vue
@@ -11,24 +11,29 @@
       <h2 id="section-welcome-title">Welcome to the ILIAD registry</h2>
       <p>The main objective of this WP is to set up an interoperable registry dedicated to rare diseases within the scope of ITHACA called <strong>ILIAD Rare Diseases patient registry</strong>: an <strong>I</strong>nternational <strong>L</strong>ibrary of <strong>I</strong>ntellectual disability and <strong>A</strong>nomalies of <strong>D</strong>evelopment.</p>
     </PageSection>
-    <PageSection aria-labelledby="quicklinks-card-title" width="full" :verticalPadding="0" :horizontalPadding="0">
+    <PageSection id="section-quick-links" width="large" aria-labelledby="quicklinks-card-title">
       <h2 class="visually-hidden" id="quicklinks-card-title">quick links</h2>
-      <div class="quicklink-cards">
-        <LinkCard id="quicklinks-about">
+      <div class="quicklink-nav">
+        <li id="quicklinks-about">
           <router-link :to="{ name: 'about-us' }">About Us</router-link>
-        </LinkCard>
-        <LinkCard id="quicklink-documents">
+          <PlusIcon />
+        </li>
+        <li id="quicklink-documents">
           <router-link :to="{ name: 'documents' }">Documents</router-link>
-        </LinkCard>
-        <LinkCard id="quicklink-dashboard">
+          <PlusIcon />
+        </li>
+        <li id="quicklink-dashboard">
           <router-link :to="{ name: 'dashboard' }">Dashboard</router-link>
-        </LinkCard>
-        <LinkCard id="quicklink-transparency">
+          <PlusIcon />
+        </li>
+        <li id="quicklink-transparency">
           <router-link :to="{ name: 'transparency' }">Transparency</router-link>
-        </LinkCard>
-        <LinkCard id="quicklink-signin">
+          <PlusIcon />
+        </li>
+        <li id="quicklink-signin">
           <a href="/login">Sign in</a>
-        </LinkCard>
+          <PlusIcon />
+        </li>
       </div>
     </PageSection>
     <PageSection id="section-aims" aria-lablledby="section-aims-title" :verticalPadding="2">
@@ -43,17 +48,23 @@
       <p>For more information on the structure of the registry, please see the <a href="/menu/main/dataexplorer?entity=sys_md_Attribute&mod=entitiesreport">Model Document (account required)</a>.</p>
     </PageSection>
   </Page>
-  <PageFooter />
+  <AppFooter />
 </template>
 
 <script>
-import { Page, PageHeader, PageSection, LinkCard } from 'rd-components'
-import PageFooter from '@/components/AppFooter.vue'
-
+import { Page, PageHeader, PageSection } from 'rd-components'
+import AppFooter from '@/components/AppFooter.vue'
+import PlusIcon from '@/components/icons/plusIcon.vue'
 import HeaderImage from '@/assets/ern-ithaca-header.jpg'
 
 export default {
-  components: { Page, PageHeader, PageSection, PageFooter, LinkCard },
+  components: {
+    Page,
+    PageHeader,
+    PageSection,
+    PlusIcon,
+    AppFooter
+  },
   data () {
     return {
       headerImage: HeaderImage
@@ -73,17 +84,63 @@ export default {
   }
 }
 
+.quicklink-nav {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 1.5em;
+  
+  li {
+    flex-grow: 1;
+    padding: 16px; 
+    text-align: center;
+    background-color: $gray-050;
+    
+    $offset: 6px;
+    box-shadow: $offset $offset 0 0 $brand-red-orange;
+    a {
+      color: $brand-blue-900;
+    }
+  }
+}
+
 #section-welcome {
   background-color: $gray-000;
 }
 
-.quicklink-cards {
-  display: flex;
-  justify-content: center;
-  align-items: center;
+#section-quick-links {
+  background-color: $brand-blue-900;
+  padding: 1em 0;
   
-  .link-card {
-    flex-grow: 1;
+  li {
+    position: relative;
+    a {
+      text-decoration: none;
+      color: currentColor;
+    }
+    svg {
+      position: absolute;
+      $size: 18px;
+      width: $size;
+      height: $size;
+      bottom: 6px;
+      right: 6px;
+      color: $brand-red-orange;
+    }
+    &:hover, &:focus {
+      color: $gray-050;
+      background-color: $brand-blue-600;
+      text-decoration: underline;
+      svg {
+        color: $gray-050;
+      }
+    }
   }
 }
+
 </style>
