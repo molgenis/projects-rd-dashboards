c77042ba0be52d57914924609d62c14efbe044c1
davidruvolo51 <davidruvolo51@gmail.com>
2022-12-02 11:01:15 +0100

    added warning message to temp pages

--- apps/ern-ithaca/src/views/AboutUs.vue
+++ apps/ern-ithaca/src/views/AboutUs.vue
@@ -1,18 +1,40 @@
 <template>
   <Page id="page-about-us">
-    <h1>Hello!</h1>
+    <PageHeader
+      id="ithaca-header"
+      title="ERN ITHACA"
+      subtitle="About Us"
+      :imageSrc="headerImage"
+      height="medium"
+      :vertical-padding="2"
+    />
+    <PageSection>
+      <MessageBox class="page-warning" type="warning">
+        <p>This page is under construction. Please check back later.</p>
+      </MessageBox>
+    </PageSection>
   </Page>
   <AppFooter />
 </template>
 
 <script>
-import { Page } from 'rd-components'
+import { Page, PageHeader, PageSection, MessageBox } from 'rd-components'
 import AppFooter from '@/components/AppFooter.vue'
+import HeaderImage from '@/assets/ern-ithaca-header.jpg'
 
 export default {
+  name: 'about-us',
   components: {
     Page,
+    PageHeader,
+    PageSection,
+    MessageBox,
     AppFooter
+  },
+  data () {
+    return {
+      headerImage: HeaderImage
+    }
   }
 }
 </script>
--- apps/ern-ithaca/src/views/Transparency.vue
+++ apps/ern-ithaca/src/views/Transparency.vue
@@ -1,18 +1,40 @@
 <template>
   <Page id="page-transparency">
-    <h1>Hello!</h1>
+    <PageHeader
+      id="ithaca-header"
+      title="ERN ITHACA"
+      subtitle="Transparency"
+      :imageSrc="headerImage"
+      height="medium"
+      :vertical-padding="2"
+    />
+    <PageSection>
+      <MessageBox class="page-warning" type="warning">
+        <p>This page is under construction. Please check back later.</p>
+      </MessageBox>
+    </PageSection>
   </Page>
   <AppFooter />
 </template>
 
 <script>
-import { Page } from 'rd-components'
+import { Page, PageHeader, PageSection, MessageBox } from 'rd-components'
 import AppFooter from '@/components/AppFooter.vue'
+import HeaderImage from '@/assets/ern-ithaca-header.jpg'
 
 export default {
+  name: 'ithaca-transparency',
   components: {
     Page,
+    PageHeader,
+    PageSection,
+    MessageBox,
     AppFooter
+  },
+  data () {
+    return {
+      headerImage: HeaderImage
+    }
   }
 }
 </script>
