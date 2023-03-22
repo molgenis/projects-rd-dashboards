b029cb56e95f136cd0cb260ed19017963baecd76
davidruvolo51 <davidruvolo51@gmail.com>
2023-03-22 12:13:35 +0100

    added new components

---
+++ apps/cosas/src/components/CheckboxGroup.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="checkbox__group">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss" >
+.checkbox__group {
+  margin: 1em 0;
+  div {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    input[type="checkbox"] {
+      margin-top: -3px;
+      margin-right: 6px;
+    }
+    label {
+      line-height: 1.4;
+    }
+  }
+}
+</style>
---
+++ apps/cosas/src/components/CheckboxInput.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="checkbox">
+    <input
+      :id="id"
+      type="checkbox"
+      class="checkbox__input"
+      v-model="selection"
+    />
+    <label :for="id" class="checkbox__label">
+      {{ label }}
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'checkbox-input',
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    value: {
+      type: Boolean,
+      default: () => false
+    },
+    label: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    selection: {
+      get () {
+        return this.value
+      },
+      set (value) {
+        this.$emit('input', value)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.checkbox {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  .checkbox__input {
+    margin-top: -3px;
+    margin-right: 6px;
+  }
+  .checkbox__label {
+    line-height: 1.4;
+  }
+}
+</style>
