519b156e8178ab2804775fb67cb8766d61119576
davidruvolo51 <davidruvolo51@gmail.com>
2023-03-31 15:57:22 +0200

    revised docs

--- apps/rd-components/src/components/VizDataValueHighlights.vue
+++ apps/rd-components/src/components/VizDataValueHighlights.vue
@@ -12,7 +12,7 @@
 <script>
 // Data highlights are used to display an interesting value for a given
 // scenario (e.g., total of *x*, recruit to date, etc.). This component
-// is designed to give a quick look -- or highlight -- variables of
+// is designed to give a quick look &mdash;or highlight of&mdash; variables of
 // interest. It is suggested to use this component at the top of a
 // dashboard and to display 3 to 4 values. It is not recommended to
 // display more than 4 or one value. Titles must be short and consise as
