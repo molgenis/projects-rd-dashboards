b9aa62da2fb8c56146687ed6dd8fc2ea584ed688
davidruvolo51 <davidruvolo51@gmail.com>
2023-04-19 14:36:20 +0200

    expanded demo dataset

--- data/institutions.py
+++ data/institutions.py
@@ -1,34 +1,67 @@
+#///////////////////////////////////////////////////////////////////////////////
+# FILE: institutions.py
+# AUTHOR: David Ruvolo
+# CREATED: 2023-04-19
+# MODIFIED: 2023-04-19
+# PURPOSE: create example dataset using ROR
+# STATUS: in.progress
+# PACKAGES: **see below**
+# COMMENTS: NA
+#///////////////////////////////////////////////////////////////////////////////
 
+from data.molgenis2 import Molgenis
+from dotenv import load_dotenv
+from datatable import dt, f
+from os import environ
 import requests
-import pandas as pd
+load_dotenv()
 
-url = "https://api.ror.org/organizations?query.advanced=country.country_name:Netherlands+AND+types:Healthcare"
-
+# get response and page numbers
+base = "https://api.ror.org/organizations"
+url = f"{base}?query.advanced=country.country_name:Netherlands+AND+(types:Healthcare+OR+types:Education+OR+types:Facility)"
 response = requests.get(url)
 json = response.json()
-data = json['items']
+json['number_of_results']
 
-df = pd.DataFrame(data)
+# get all pages
+rawdata = []
+pages = list(range(0, int(round(json['number_of_results'] / 20, 0))))
 
-keepColumns = [
-  'id',
-  'name',
-  'addresses'
-]
+for pageNum in pages:
+  print(f"Retrieving data for page: {pageNum+1}")
+  pageUrl = f"{url}&page={pageNum+1}"
+  pageResponse = requests.get(pageUrl)
+  pageResponse.raise_for_status()
+  if pageResponse:
+    pageJson = pageResponse.json()
+    pageData = pageJson['items']
+    rawdata.extend(pageData)
 
-for column in df.columns:
-  if column not in keepColumns:
-    del df[column]
-    
-df['lat'] = df['addresses'].apply(lambda x: x[0]['lat'])
+# extract row level data
+data = []
+for row in rawdata:
+  data.append({
+    'id': row['id'],
+    'name': row['name'],
+    'lat': row['addresses'][0]['lat'],
+    'lng': row['addresses'][0]['lng'],
+    'city': row['addresses'][0]['city'],
+    'province': row['addresses'][0]['geonames_city']['nuts_level2']['name'],
+    'type': ','.join(row['types'])
+  })
 
-df['lng'] = df['addresses'].apply(lambda x: x[0]['lng'])
+# prep and import
+locationsDT = dt.Frame(data)
+locationsDT['province'] = dt.Frame([
+  value.replace('(NL)','').strip() if value else value
+  for value in locationsDT['province'].to_list()[0]
+])
 
-df['city'] = df['addresses'].apply(
-  lambda x: x[0]['city']
+
+db = Molgenis(environ['MOLGENIS_PROD_HOST'])
+db.login(environ['MOLGENIS_PROD_USR'], environ['MOLGENIS_PROD_PWD'])
+
+db.importDatatableAsCsv(
+  pkg_entity='rdcomponents_institutions',
+  data=locationsDT
 )
-
-
-del df['addresses']
-
-df.to_csv('data/institutions.csv',index=False)
