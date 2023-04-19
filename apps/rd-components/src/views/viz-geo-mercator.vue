476c12d8f20fa2eb741188763272f3d4d5cdf129
davidruvolo51 <davidruvolo51@gmail.com>
2023-04-19 15:18:47 +0200

    updated example page

--- apps/rd-components/src/views/viz-geo-mercator.vue
+++ apps/rd-components/src/views/viz-geo-mercator.vue
@@ -2,7 +2,7 @@
   <Page>
     <PageSection class="viz-section">
       <h2>GeoMercator Component</h2>
-      <p>The <strong>GeoMercator</strong> component can be used to create a point location visualation using a geomercator map from the D3 library where each point represents a unique location in the dataset. The map can be customised by adjusting the properties or using CSS.</p>
+      <p>The <strong>GeoMercator</strong> component can be used to create a point location visualisation using a geomercator map from the D3 library where each point represents a unique location in the dataset. The map can be customised by adjusting the properties or using CSS.</p>
       <MessageBox v-if="loading & !hasError">
         <p>Fetching data</p>
       </MessageBox>
@@ -17,16 +17,18 @@
         :chartData="data"
         latitude="lat"
         longitude="lng"
-        groupingVariable="group" 
+        groupingVariable="type" 
         :groupColorMappings="mapColorGroups"
         :legendData="mapColorGroups"
-        :chartScale="6"
-        :mapCenter="{latitude: 4.515, longitude: 52.205}"
+        :chartScale="2"
+        :mapCenter="{latitude: 4.515, longitude: 49.205}"
+        :zoomLimits="[0.2, 25]"
         :tooltipTemplate="(row) => {
           return `
             <p class='title'>${row.name}</p>
             <p class='location'>
               <span class='location-city'>${row.city}</span>
+              <span class='location-city'>${row.type}</span>
             </p>
           `
         }"
@@ -43,6 +45,7 @@ import GeoMercator from '@/components/VizGeoMercator.vue'
 
 import { fetchData } from '$shared/js/utils.js'
 import geojson from '$shared/data/world.geo.json'
+// import geojson from '$shared/data/world.50.geo.json'
 
 export default {
   components: {
@@ -59,19 +62,17 @@ export default {
       data: {},
       geojson: geojson,
       mapColorGroups: {
-        'Group A': 'pink',
-        'Group B': 'cornflowerblue',
-        'Group C': 'yellow',
+        'Education': '#F4D58D',
+        'Facility': '#FC7573',
+        'Healthcare': '#B6C2D9',
       }
     }
   },
   mounted () {
     Promise.resolve(
-      fetchData('/api/v2/rdcomponents_institutions')
+      fetchData('/api/v2/rdcomponents_institutions?num=5000&q=lat!=""')
     ).then(response => {
       this.data = response.items
-      const groups = ['Group A', 'Group B', 'Group C']
-      this.data.map(row => row['group'] = groups[Math.floor(Math.random()*groups.length)])
       this.loading = false
     }).catch(error => {
       this.loading = false
