a02f186382035af475921819a76f48502aab6b68
davidruvolo51 <davidruvolo51@gmail.com>
2023-04-19 15:17:42 +0200

    increased size of example dataset

--- data/institutions.py
+++ data/institutions.py
@@ -16,39 +16,51 @@ from os import environ
 import requests
 load_dotenv()
 
-# get response and page numbers
 base = "https://api.ror.org/organizations"
-url = f"{base}?query.advanced=country.country_name:Netherlands+AND+(types:Healthcare+OR+types:Education+OR+types:Facility)"
-response = requests.get(url)
-json = response.json()
-json['number_of_results']
 
-# get all pages
-rawdata = []
-pages = list(range(0, int(round(json['number_of_results'] / 20, 0))))
-
-for pageNum in pages:
-  print(f"Retrieving data for page: {pageNum+1}")
-  pageUrl = f"{url}&page={pageNum+1}"
-  pageResponse = requests.get(pageUrl)
-  pageResponse.raise_for_status()
-  if pageResponse:
-    pageJson = pageResponse.json()
-    pageData = pageJson['items']
-    rawdata.extend(pageData)
-
-# extract row level data
+# get response and page numbers
 data = []
-for row in rawdata:
-  data.append({
-    'id': row['id'],
-    'name': row['name'],
-    'lat': row['addresses'][0]['lat'],
-    'lng': row['addresses'][0]['lng'],
-    'city': row['addresses'][0]['city'],
-    'province': row['addresses'][0]['geonames_city']['nuts_level2']['name'],
-    'type': ','.join(row['types'])
-  })
+
+countries = ['Netherlands', 'Germany', 'Belgium', 'France','Luxembourg']
+for country in countries:
+  print(f'Pulling data for {country}....')
+  url = f"{base}?query.advanced=country.country_name:{country}+AND+(types:Healthcare+OR+types:Education+OR+types:Facility)"
+  response = requests.get(url)
+  json = response.json()
+  print(f"Retrieving: {json['number_of_results']} results")
+
+  # get all pages
+  rawdata = []
+  pages = list(range(0, int(round(json['number_of_results'] / 20, 0))))
+
+  for pageNum in pages:
+    print(f"Retrieving data for page: {pageNum+1}")
+    pageUrl = f"{url}&page={pageNum+1}"
+    pageResponse = requests.get(pageUrl)
+    pageResponse.raise_for_status()
+    if pageResponse:
+      pageJson = pageResponse.json()
+      pageData = pageJson['items']
+      rawdata.extend(pageData)
+
+  # extract row level data
+  for row in rawdata:
+    data.append({
+      'id': row['id'],
+      'name': row['name'],
+      'lat': row['addresses'][0]['lat'],
+      'lng': row['addresses'][0]['lng'],
+      'city': row['addresses'][0]['city'],
+      'province': row['addresses'][0].get('geonames_city',{}).get('nuts_level2'),
+      'type': ','.join(row['types'])
+    })
+
+
+for row in data:
+  if row.get('province'):
+    row['province'] =row['province'].get('name')
+  else:
+    row['province'] = None
 
 # prep and import
 locationsDT = dt.Frame(data)
