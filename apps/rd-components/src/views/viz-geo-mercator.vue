5df270248e58578e1f830ad983db4696ea726404
davidruvolo51 <davidruvolo51@gmail.com>
2023-04-20 11:44:59 +0200

    added new map events

--- apps/rd-components/src/views/viz-geo-mercator.vue
+++ apps/rd-components/src/views/viz-geo-mercator.vue
@@ -15,7 +15,7 @@
       <h2>Map Example</h2>
       <p>The map component can be used to create a point location visualisation using a geomercator map from the D3 library where each point represents a unique location in the dataset. The map can be customised by adjusting the properties or using CSS.</p>
     </PageSection>
-    <PageSection class="bkg-light" :verticalPadding="3">
+    <PageSection class="bkg-light" :verticalPadding="2">
       <MessageBox v-if="loading & !hasError">
         <p>Fetching data</p>
       </MessageBox>
@@ -25,6 +25,8 @@
       <GeoMercator 
         v-else
         chartId="map"
+        title="ROR Organisations"
+        description="The following map shows organisations in Belgium, France, Germany, and the Netherlands by type (as defined in ROR: education, facility, and healthcare). Zoom in to explore the map and hover over a location to view more information. Click a location to view the row level data below. Click an item in the legend to hide or show the data on the map."
         rowId="name"
         :geojson="geojson"
         :chartData="data"
@@ -41,11 +43,19 @@
             <p class='title'>${row.name}</p>
             <p class='location'>
               <span class='location-city'>${row.city}</span>
-              <span class='location-city'>${row.type}</span>
+              <span class='location-type'>${row.type}</span>
             </p>
           `
         }"
-      />
+        :enableLegendClicks="true"
+        :enableMarkerClicks="true"
+        @markerClick="(value) => updateSelection(value)"
+        />
+    </PageSection>
+    <PageSection>
+    <output class="output">
+      {{  location }}
+    </output>
     </PageSection>
   </Page>
 </template>
@@ -84,14 +94,22 @@ export default {
         'Education': '#F4D58D',
         'Facility': '#FC7573',
         'Healthcare': '#B6C2D9',
-      }
+      },
+      location: null
+    }
+  },
+  methods: {
+    updateSelection (data) {
+      this.location = data
     }
   },
   mounted () {
     Promise.resolve(
       fetchData('/api/v2/rdcomponents_institutions?num=5000&q=lat!=""')
     ).then(response => {
-      this.data = response.items
+      const data = response.items
+      data.forEach(row => delete row['_href'])
+      this.data = data
       this.loading = false
     }).catch(error => {
       this.loading = false
