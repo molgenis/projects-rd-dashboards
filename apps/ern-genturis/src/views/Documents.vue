daaa33e3bdd64dbf6fbad62c441ff5102058ef91
davidruvolo51 <davidruvolo51@gmail.com>
2023-06-15 16:08:02 +0200

    added loop to render available files; updated theme

--- apps/ern-genturis/src/views/Documents.vue
+++ apps/ern-genturis/src/views/Documents.vue
@@ -9,18 +9,32 @@
       titlePositionX="center"
       titlePositionY="center"
     />
-    <PageSection id="genturis-section-documents" aria-labelledby="genturis-section-documents-title" width="large">
+    <PageSection
+      id="genturis-section-documents"
+      aria-labelledby="genturis-section-documents-title"
+      width="large"
+    >
       <h2 id="genturis-section-documents-title">Documents</h2>
       <p>Download additional information about the GENTURIS Registry.</p>
       <div class="document-container">
         <MessageBox type="error" v-if="!loading && apiError">
-          <p>Unable to retrieve file information. Please sign in or try again later.</p>
+          <p>
+            Unable to retrieve file information. Please sign in or try again
+            later.
+          </p>
         </MessageBox>
         <ul class="document-list" v-else>
-          <li>
-            <a :href="GenturisRegistryLeaflet.url">
-              {{ GenturisRegistryLeaflet.filename }}
-              (PDF, {{ Math.round(GenturisRegistryLeaflet.size / 1000) }} KB)
+          <li class="file" v-for="file in files" :key="file.id">
+            <p class="file-element file-name">{{ file.filename }}</p>
+            <p class="file-element file-format">
+              {{ file.contentType.split("/")[1].toUpperCase() }}
+            </p>
+            <p class="file-element file-size">
+              {{ Math.round(file.size / 1000) }} KB
+            </p>
+            <a class="file-element file-url" :href="file.url">
+              <span class="visually-hidden">Download {{ file.filename }}</span>
+              <DownloadIcon />
             </a>
           </li>
         </ul>
@@ -30,46 +44,91 @@
 </template>
 
 <script>
-import { Page, PageHeader, PageSection, MessageBox, LoadingScreen } from 'rd-components'
-import GenturisHeaderPhoto from '@/assets/genturis-carousel.jpg'
-import { fetchData, subsetData } from '$shared/js/utils.js'
+import {
+  Page,
+  PageHeader,
+  PageSection,
+  MessageBox,
+  LoadingScreen
+} from "rd-components";
+import GenturisHeaderPhoto from "@/assets/genturis-carousel.jpg";
+import DownloadIcon from "@/components/icons/arrow-down-tray.vue";
+import { fetchData } from "$shared/js/utils.js";
 
 export default {
-  name: 'page-documents',
+  name: "page-documents",
   components: {
     Page,
     PageHeader,
     PageSection,
     MessageBox,
-    LoadingScreen
+    LoadingScreen,
+    DownloadIcon
   },
-  data () {
+  data() {
     return {
       loading: true,
       apiError: null,
       GenturisHeaderPhoto: GenturisHeaderPhoto,
       GenturisRegistryLeaflet: null
-    }
+    };
   },
-  mounted () {
-    Promise.resolve(
-      fetchData('/api/v2/sys_FileMeta')
-    ).then(response => { 
-      const files = response.items
-      this.GenturisRegistryLeaflet=subsetData(files, 'filename', 'GENTURIS_Registry_Leaflet.pdf')[0]
-      this.loading = false
-    }).catch(error => {
-      this.apiError = error
-    })
+  mounted() {
+    Promise.resolve(fetchData("/api/v2/sys_FileMeta?q=(filename=notlike=final2;filename=like=genturis)"))
+      .then(response => {
+        const files = response.items;
+        this.files = files;
+      })
+      .then(() => {
+        this.loading = false;
+      })
+      .catch(error => {
+        this.apiError = error;
+      });
   }
-}
+};
 </script>
 
 <style lang="scss">
 .document-list {
-  li {
-    a {
-      text-decoration: none;
+  list-style: none;
+  padding: 0;
+  margin: 0 auto;
+  
+
+  .file {
+    display: grid;
+    grid-template-columns: 0.8fr 0.2fr 0.2fr 0.1fr;
+    justify-content: center;
+    align-items: stretch;
+    box-shadow: $box-shadow;
+    box-sizing: border-box;
+    margin-bottom: 2em;
+    padding: 0;
+    margin: 0;
+    margin-bottom: 2em;
+
+    .file-element {
+      padding: 1.75em 1em;
+      margin: 0;
+      background-color: $red-050;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+    
+    .file-name {
+      justify-content: flex-start;
+    }
+    
+    .file-url {
+      background-color: $blue-050;
+      color: $blue-800;
+      
+      &:hover, &:focus {
+        background-color: $blue-800;
+        color: $blue-050;
+      }
     }
   }
 }
