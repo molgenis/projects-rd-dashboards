2202eb14fd78b8d10cd18bfed0b97bf17ba5c373
dcruvolo <davidruvolo51@gmail.com>
2023-10-31 16:11:52 +0100

    fix: resolved incorrect link

--- apps/ern-genturis/src/views/HomePage.vue
+++ apps/ern-genturis/src/views/HomePage.vue
@@ -48,7 +48,7 @@
     </PageSection>
     <PageSection id="genturis-section-contact" aria-labelledby="genturis-section-contact-title" :verticalPadding="2" width="large">
       <h2 id="genturis-section-contact-title">Contact</h2>
-      <p>For further information, please contact the GENTURIS registry coordination team by email <a href="mailto:genturis-registry@radboudumc.nl">genturis-registry@radboudumc.nl</a> or at the following address.</p>
+      <p>For further information, please contact the GENTURIS registry coordination team by email <a href="mailto:genturis-registry@umcg.nl">genturis-registry@umcg.nl</a> or at the following address.</p>
       <GenturisAddress :centerText="true" />
     </PageSection>
   </Page>
