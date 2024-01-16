6940fd7473b46db8f4601600f0293b9a25b0c3f4
dcruvolo <davidruvolo51@gmail.com>
2024-01-16 10:19:56 +0100

    feat: updated header image

--- apps/ern-skin/src/views/HomePage.vue
+++ apps/ern-skin/src/views/HomePage.vue
@@ -31,11 +31,9 @@
       <h2 id="section-aims-title">Aims of the ERN-Skin registry</h2>
       <p>The <strong>ERN-Skin</strong> gathers European medical experts and patient representatives who join their efforts to improve the diagnosis and management of all the patients suffering from rare, complex and undiagnosed skin diseases all over Europe.</p>
       <p>Structured in 9 Disease Groups and Transversal thematic Groups, ERN-Skin has been developing an innovative e-health platform allowing the construction of a common Skin Diseases registry: ERRAS, a key instrument for our ERN.</p>
-      <!-- insert image: ern-skin box diagram -->
       <img :src="ernSkinDiagram" class="project-diagrams" alt="ERN-Skin disease groups"/>
       <p>The SKIN ePAGs bring together rare skin disease patient representatives who are actively involved in the work of the ERN-Skin.</p>
       <p>ERRAS, the European registry for rare dermatologic diseases receives information from existing European registries as well as additional manual feeds. It is a means to put scientists and patients together via their respective healthcare providers.</p>
-      <!-- insert image: ERDI image -->
       <img :src="ernSkinErdri" class="project-diagrams" alt="European registry for rare dermatologic diseases" />
     </PageSection>
     <PageSection id="section-contact" aria-labelledby="section-contact-title" :verticalPadding="2">
@@ -50,7 +48,7 @@
 import { Page, PageHeader, PageSection } from 'rd-components'
 import ProjectAddress from '@/components/ProjectAddress.vue'
 
-import headerImage from '@/assets/app-header-background.png'
+import headerImage from '@/assets/app-header-background.jpg'
 import ernSkinDiagram from '@/assets/ern-skin-groups.png'
 import ernSkinErdri from '@/assets/ern-skin-erdri.png'
 
